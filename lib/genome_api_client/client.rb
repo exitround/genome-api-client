d8165bafa7d657783048d1f780e59be73366d35b
Greg Dean <dean.greg@gmail.com>
2015-11-20 09:49:46 -0800

    parsing response

--- lib/genome_api_client/client.rb
+++ lib/genome_api_client/client.rb
@@ -10,15 +10,15 @@ module GenomeApi
     end
 
     def user
-      self.class.get('/user')
+      self.class.get('/user').parsed_response
     end
 
     def users(id = nil)
-      self.class.get("/users/#{id}")
+      self.class.get("/users/#{id}").parsed_response
     end
 
     def create_user(params = {})
-      self.class.post('/users', body:params)
+      self.class.post('/users', body:params).parsed_response
     end
   end
 end
